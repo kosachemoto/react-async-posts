9f1c2c049d0514d35144b48487f6cd8519e1e178
Nikita Kosachev <kosachev.a.nikita@gmail.com>
2019-02-19 03:17:40 +0300

    Add «PostDetail» component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export * from './post';
 export * from './post-list';
+export * from './post-detail';
 export * from './more-button';
 export * from './app';
 export * from './root';
---
+++ src/components/post-detail/post-detail.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './post-detail.css';
+
+export const PostDetail = ({className}) => {
+  return (
+    <div className={`${className} post-detail`}>
+      PostDetail-component
+    </div>
+  );
+}
