252f2bb138c6670a3a2e38a970337f449094eba9
Nikita Kosachev <kosachev.a.nikita@gmail.com>
2019-02-20 03:28:32 +0300

    Add «CommentList» component

---
+++ src/components/comment-list/comment-list.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Comment } from './../../components';
+import './comment-list.css';
+
+export const CommentList = () => {
+  return (
+    <div className="comment-list">
+      <Comment />
+      <Comment />
+      <Comment />
+    </div>
+  );
+}
