1f5b62f46c11af26aaedd5d33d06aeb1c53155b7
Nikita Kosachev <kosachev.a.nikita@gmail.com>
2019-02-22 02:50:39 +0300

    Add some styles for more-button

--- src/components/more-button/more-button.jsx
+++ src/components/more-button/more-button.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './more-button.css';
+import plus from './../../assets/svg/plus.svg';
 
 export const MoreButton = ({loadMore}) => {
   return (
     <div className="more-button" onClick={loadMore}>
-      Load More
+      <div className="more-button__title">Load More</div>
+      <img className="more-button__img" src={plus} alt="Add more"/>
     </div>
   )
 }
