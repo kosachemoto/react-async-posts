34478acc5e988f9979a59025732f5ca9bf7e5448
Nikita Kosachev <kosachev.a.nikita@gmail.com>
2019-02-27 03:09:54 +0300

    Fix error handling for posts request

--- src/actions.js
+++ src/actions.js
@@ -48,16 +48,14 @@ function requestComments(stage, payload) {
 export function receivePost(postId) {
   return function (dispatch) {
     dispatch(requestPost('START'));
-    try {
-      services.Post.post(postId)
-        .then(post => {
-          console.log("post:", post);
-          dispatch(requestPost('FINISH', { post }))
-        })
-    } catch (error) {
-      services.Post.loaded = false;
-      dispatch(requestPost('ERROR', { error }))
-    }
+    services.Post.post(postId)
+      .then(post => {
+        console.log("post:", post);
+        dispatch(requestPost('FINISH', { post }))
+      })
+      .catch(error => {
+        dispatch(requestPost('ERROR', {error}));
+      })
   }
 }
 
