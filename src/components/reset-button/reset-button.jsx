d9adb833d391f64ff149953b9d91f267ba46043f
Nikita Kosachev <kosachev.a.nikita@gmail.com>
2019-03-02 05:04:27 +0300

    Update SVG icons

--- src/components/reset-button/reset-button.jsx
+++ src/components/reset-button/reset-button.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './reset-button.css';
+import stop from './../../assets/svg/stop.svg';
 
 export const ResetButton = ({resetPosts}) => {
   return (
     <div className="reset-button" onClick={resetPosts}>
-      <div className="reset-button__title">Reset Local Storage</div>
+      <div className="reset-button__title">Reset Store</div>
+      <img className="reset-button__img" src={stop} alt="Reset store"/>
     </div>
   )
 }
